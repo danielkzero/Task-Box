618d0c0db404c1468229b544da5be113c5fb5f6e
Daniel S. Ramos <danikzero@hotmail.com>
2025-10-27 13:15:54 -0300

    Utilizando moment.js

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,12 +1,13 @@
+import moment from 'moment';
+import { TaskDetails } from './TaskDetails';
 export class Task {
   constructor(
     public id?: number,
     public title: string = "",
     public done: boolean = false,
-    public listId?: number, // Antes categoryId
-    public createdAt: Date = new Date(),
+    public listId?: number, 
+    public createdAt: Date = moment(new Date()).local().toDate(),
     public scheduledFor?: Date
   ) {}
-
 }
 
