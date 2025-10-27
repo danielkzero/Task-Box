228ae978cb4a6d21518639ef9fed0504b5cc11ce
Daniel S. Ramos <danikzero@hotmail.com>
2025-10-27 15:43:51 -0300

    Modificando css para adicionar uma área segura no topo, não consegui testar para ver se deu certo.

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,10 @@
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
+<style>
+ion-header,
+ion-toolbar,
+ion-content {
+  padding-top: var(--ion-safe-area-top, 0px);
+}
+</style>
